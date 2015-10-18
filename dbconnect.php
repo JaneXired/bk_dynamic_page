61458490dcc0926238d6c03964dac930b3979c29
Janine Derix <j.derix@wbl-konzept.de>
2015-10-18 13:10:48 +0200

    merge problems fixed

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,10 @@
 <?php
+
     $connect = mysql_connect("localhost", "root", "");
     if($connect == false) {
 	    $connect = mysql_connect("localhost", "cjn_", "cjn_meo3") or die("Die Datenbank ist momentan nicht erreichbar");
 	}
+
 	//echo "select_db = " . mysql_select_db('meo3', $connect) . "<br/>";
 
 	//$query = 'SELECT * FROM umfrage';
@@ -16,4 +18,5 @@
 	//else {
 	  //  echo 'NOPE';
 	//}
+
 ?>
--- umfrage.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	$query = 'SELECT id=3 FROM umfrage';
-	$result = mysql_query($query);
-	echo $result;
- ?>
-
