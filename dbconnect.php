346c4d15aa7726576030374ac060cc805965eb8d
Janine Derix <j.derix@wbl-konzept.de>
2015-10-18 14:14:53 +0200

    fixed the db connection and now answer is send to db

--- dbconnect.php
+++ dbconnect.php
@@ -3,20 +3,11 @@
     $connect = mysql_connect("localhost", "root", "");
     if($connect == false) {
 	    $connect = mysql_connect("localhost", "cjn_", "cjn_meo3") or die("Die Datenbank ist momentan nicht erreichbar");
-	}
+	    echo "connected to cjn";
+	} else {
+	    echo"connected to localhost";
+    }
 
-	//echo "select_db = " . mysql_select_db('meo3', $connect) . "<br/>";
-
-	//$query = 'SELECT * FROM umfrage';
-	//$resource = mysql_query($query);
-	//$result = mysql_fetch_assoc($resource); 
-	//if(is_array($result)) {
-	  //   foreach($result as $key => $value){
-	     //   echo "row: " . $key . " = " .  $value . '<br/>';
-	     //}  
-	//}
-	//else {
-	  //  echo 'NOPE';
-	//}
+	mysql_select_db("meo3", $connect);
 
 ?>
--- results.php
+++ results.php
@@ -12,6 +12,42 @@
 Test, geschafft!
 
 <?php
+
+
+	$gruppe = $_POST["gruppe"];
+
+$antwort1 = 0;
+$antwort2 = 0;
+$antwort3 = 0;
+$antwort4 = 0;
+$antwort5 = 0;
+
+
+	if($gruppe=="1") {
+		$antwort1=1;
+	}
+	else if($gruppe=="2") {
+		$antwort2=1;
+	}
+	else if($gruppe=="3") {
+		$antwort3=1;
+	}
+	else if($gruppe=="4") {
+		$antwort4=1;
+	}
+	else if($gruppe=="5") {
+		$antwort5=1;
+	}
+
+		$eintrag = "INSERT INTO umfrage (antwort_1, antwort_2, antwort_3, antwort_4, antwort_5) VALUES (" . $antwort1 .", ". $antwort2 .", ". $antwort3 .", ". $antwort4 .", ". $antwort5 .");";
+echo $eintrag;
+		$eintragen = mysql_query($eintrag);
+		echo "<p>" . $eintrag . "</p>";
+
+
+
+
+
     $query = "SELECT * FROM 'umfrage'";
     $doit = mysql_query($query);   
     echo $doit;
